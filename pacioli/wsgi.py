af253db9153e785d0047b0b3cfbb0196c8fa5e06
Pierre Rochard <pierre@rochard.org>
2015-11-17 18:14:51 -0500

    add logging

--- pacioli/wsgi.py
+++ pacioli/wsgi.py
@@ -1,7 +1,10 @@
 import os
+import sys
+import logging
 
 from pacioli import create_app
 
+logging.basicConfig(stream=sys.stderr)
 
 env = os.environ.get('pacioli_ENV', 'dev')
 app = create_app('pacioli.settings.%sConfig' % env.capitalize(), env=env)
