663f1c038d7e1fa514353444028bff58549fcd34
Pierre Rochard <pierre@rochard.org>
2015-11-23 08:17:24 -0500

    fixing ofx

--- plugins/ofx.py
+++ plugins/ofx.py
@@ -11,7 +11,7 @@ from ofxtools.ofxalchemy.models import STMTTRN
 
 script_path = os.path.realpath(__file__)
 pacioli_path = os.path.join(script_path, '../pacioli/')
-sys.path.insert(0, pacioli_path)
+sys.path.insert(1, pacioli_path)
 
 from db_config import PROD_PG_USERNAME, PROD_PG_PASSWORD, PROD_PG_HOST, PROD_PG_PORT
 from ofx_config import url, org, fid, bankid_checking, bankid_savings, checking, savings, creditcard, user, password
