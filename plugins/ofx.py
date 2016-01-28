56e6f2aba46f3549be7279e56e65b642ebe5b8ce
Pierre Rochard <pierre@rochard.org>
2016-01-27 19:36:12 -0500

    updating to clientuid

--- plugins/ofx.py
+++ plugins/ofx.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
+import argparse
 from datetime import datetime
 
 from ofxtools import OFXClient
@@ -8,7 +9,7 @@ from ofxtools.ofxalchemy import DBSession, OFXParser, Base
 from ofxtools.ofxalchemy.models import STMTTRN
 
 from ofx_config import url, org, fid, bankid_checking, bankid_savings
-from ofx_config import checking, savings, creditcard, user, password
+from ofx_config import checking, user, password, clientuid, savings, creditcard
 from ofx_config import PROD_PG_USERNAME, PROD_PG_PASSWORD, PROD_PG_HOST, PROD_PG_PORT
 
 
@@ -19,15 +20,21 @@ engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)
 DBSession.configure(autocommit=True, autoflush=True, bind=engine)
 client = OFXClient(url, org, fid)
 
-accounts = [BankAcct(bankid_checking, checking, 'checking'), BankAcct(bankid_savings, savings, 'savings'), CcAcct(creditcard)]
+accounts = [BankAcct(bankid_checking, checking, 'checking'),
+            BankAcct(bankid_savings, savings, 'savings'),
+            CcAcct(creditcard)]
 
 
-def setup():
+def setup(drop_tables):
+    if drop_tables:
+        Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     for account in accounts:
-        request = client.statement_request(user, password, [account])
+        print(account._acct['ACCTTYPE'])
+        request = client.statement_request(user, password, clientuid, [account])
         response = client.download(request)
         parser = OFXParser()
+        response.seek(0)
         parser.parse(response)
         parser.instantiate()
 
@@ -36,7 +43,8 @@ def update():
     start, = DBSession.query(STMTTRN.dtposted).order_by(STMTTRN.dtposted.desc()).first()
     end = datetime.today()
     for account in accounts:
-        request = client.statement_request(user, password, [account], dtstart=start, dtend=end)
+        print(account._acct['ACCTTYPE'])
+        request = client.statement_request(user, password, clientuid, [account], dtstart=start, dtend=end)
         response = client.download(request)
         parser = OFXParser()
         parser.parse(response)
@@ -44,5 +52,12 @@ def update():
 
 
 if __name__ == '__main__':
-    # setup()
-    update()
+    ARGS = argparse.ArgumentParser()
+    ARGS.add_argument('-s', action='store_true', dest='setup', default=False)
+    ARGS.add_argument('-u', action='store_true', dest='update', default=True)
+    ARGS.add_argument('-d', action='store_true', dest='drop_tables', default=False)
+    args = ARGS.parse_args()
+    if args.setup:
+        setup(args.drop_tables)
+    if args.update:
+        update()
