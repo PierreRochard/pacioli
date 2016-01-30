4a77f5647750109a02e8b7577ea42e670dba8e33
Pierre Rochard <pierre@rochard.org>
2016-01-30 12:04:43 -0500

    cleaning up ofx

---
+++ plugins/ofx_config_example.py
@@ -0,0 +1,18 @@
+checking = ''
+savings = ''
+creditcard = ''
+
+user = ''
+password = ''
+clientuid = ''
+
+PROD_PG_USERNAME = ''
+PROD_PG_PASSWORD = ''
+PROD_PG_HOST = ''
+PROD_PG_PORT = 5432
+
+
+MAIL_USERNAME = 'you@gmail.com'
+MAIL_PASSWORD = ''
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
