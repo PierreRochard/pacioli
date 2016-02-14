d6ce8cf838a732b90fd287f27067b8ccc1de5229
Pierre Rochard <pierre@rochard.org>
2016-02-14 15:26:13 -0500

    adding amazon schema

--- manage.py
+++ manage.py
@@ -50,6 +50,10 @@ def createdb():
         db.engine.execute('CREATE SCHEMA pacioli;')
     except ProgrammingError:
         pass
+    try:
+        db.engine.execute('CREATE SCHEMA amazon;')
+    except ProgrammingError:
+        pass
 
     db.create_all()
     OFX_Base.metadata.create_all(db.engine)
