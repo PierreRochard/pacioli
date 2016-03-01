d6d30555cf943c1d7ee9811f41186525017dd332
Pierre Rochard <pierre@rochard.org>
2016-02-29 19:07:59 -0500

    removing time zone

--- pacioli/models.py
+++ pacioli/models.py
@@ -23,10 +23,10 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
 
-    confirmed_at = db.Column(db.DateTime(timezone=True))
+    confirmed_at = db.Column(db.DateTime())
 
-    last_login_at = db.Column(db.DateTime(timezone=True))
-    current_login_at = db.Column(db.DateTime(timezone=True))
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
     last_login_ip = db.Column(db.String(255))
     current_login_ip = db.Column(db.String(255))
     login_count = db.Column(db.Integer)
