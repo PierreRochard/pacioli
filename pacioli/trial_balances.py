fdced8c1959eaa3110082d4e9a0fe1d9cd9a42a7
Pierre Rochard <pierre@rochard.org>
2016-03-12 09:28:02 -0500

    corrected bug

--- pacioli/trial_balances.py
+++ pacioli/trial_balances.py
@@ -12,7 +12,7 @@ def create_trigger_function():
         db.engine.execute("""
         CREATE FUNCTION pacioli.subaccount_insert(new pacioli.journal_entries) RETURNS VOID AS $$
           DECLARE
-          period_intervals VARCHAR[5] := '{"YYYY-MM-DD", "YYYY-WW", "YYYY-MM", "YYYY-Q", "YYYY"}';
+          period_intervals VARCHAR[5] := '{"YYYY", "YYYY-Q", "YYYY-MM", "YYYY-WW", "YYYY-MM-DD"}';
           period_interval_name VARCHAR;
           period_name RECORD;
           existing_debit_record RECORD;
@@ -26,41 +26,48 @@ def create_trigger_function():
               FOREACH period_interval_name IN ARRAY period_intervals
               LOOP
                 <<periods_loop>>
-                FOR period_name in SELECT to_char(timestamp, period_interval_name) AS p from pacioli.journal_entries GROUP BY to_char(timestamp, period_interval_name) LOOP
+                FOR period_name in SELECT DISTINCT to_char(timestamp, period_interval_name) AS p from pacioli.journal_entries LOOP
 
-                  SELECT sum(functional_amount) INTO debit_debit_balance FROM pacioli.journal_entries WHERE debit_subaccount = new.debit_subaccount AND to_char(timestamp, period_interval_name) <= period_name.p;
+                  SELECT sum(functional_amount) INTO debit_debit_balance FROM pacioli.journal_entries WHERE debit_subaccount = new.debit_subaccount
+                                                                                                        AND to_char(timestamp, period_interval_name) <= period_name.p;
                   IF debit_debit_balance IS NULL THEN
                     debit_debit_balance := 0;
                   END IF;
 
-                  SELECT sum(functional_amount) INTO debit_changes_amount FROM pacioli.journal_entries WHERE debit_subaccount = new.debit_subaccount AND to_char(timestamp, period_interval_name) = period_name.p;
+                  SELECT sum(functional_amount) INTO debit_changes_amount FROM pacioli.journal_entries WHERE debit_subaccount = new.debit_subaccount
+                                                                                                         AND to_char(timestamp, period_interval_name) = period_name.p;
                   IF debit_changes_amount IS NULL THEN
                     debit_changes_amount := 0;
                   END IF;
 
-                  SELECT * INTO existing_debit_record FROM pacioli.trial_balances WHERE pacioli.trial_balances.subaccount = new.debit_subaccount AND pacioli.trial_balances.period = period_name.p;
+                  SELECT * INTO existing_debit_record FROM pacioli.trial_balances WHERE pacioli.trial_balances.subaccount = new.debit_subaccount
+                                                                                    AND pacioli.trial_balances.period = period_name.p
+                                                                                    AND pacioli.trial_balances.period_interval = period_interval_name;
                   IF existing_debit_record IS NULL THEN
                     INSERT INTO pacioli.trial_balances (subaccount, debit_balance, credit_balance, debit_changes, credit_changes, period, period_interval)
-                      VALUES (new.debit_subaccount, debit_debit_balance, 0, debit_changes_amount, 0, period_name.p, period_interval_name);
-
+                                                VALUES (new.debit_subaccount, debit_debit_balance, 0, debit_changes_amount, 0, period_name.p, period_interval_name);
                   ELSE
                     UPDATE pacioli.trial_balances SET debit_balance = debit_debit_balance, debit_changes = debit_changes_amount WHERE id = existing_debit_record.id;
                   END IF;
 
-                  SELECT sum(functional_amount) INTO credit_credit_balance FROM pacioli.journal_entries WHERE credit_subaccount = new.credit_subaccount AND to_char(timestamp, period_interval_name) <= period_name.p;
+                  SELECT sum(functional_amount) INTO credit_credit_balance FROM pacioli.journal_entries WHERE credit_subaccount = new.credit_subaccount
+                                                                                                          AND to_char(timestamp, period_interval_name) <= period_name.p;
                   IF credit_credit_balance IS NULL THEN
                     credit_credit_balance := 0;
                   END IF;
 
-                  SELECT sum(functional_amount) INTO credit_changes_amount FROM pacioli.journal_entries WHERE credit_subaccount = new.credit_subaccount AND to_char(timestamp, period_interval_name) = period_name.p;
+                  SELECT sum(functional_amount) INTO credit_changes_amount FROM pacioli.journal_entries WHERE credit_subaccount = new.credit_subaccount
+                                                                                                          AND to_char(timestamp, period_interval_name) = period_name.p;
                   IF credit_changes_amount IS NULL THEN
                     credit_changes_amount := 0;
                   END IF;
 
-                  SELECT * INTO existing_credit_record FROM pacioli.trial_balances WHERE pacioli.trial_balances.subaccount = new.credit_subaccount AND pacioli.trial_balances.period = period_name.p;
+                  SELECT * INTO existing_credit_record FROM pacioli.trial_balances WHERE pacioli.trial_balances.subaccount = new.credit_subaccount
+                                                                                     AND pacioli.trial_balances.period = period_name.p
+                                                                                     AND pacioli.trial_balances.period_interval = period_interval_name;
                   IF existing_credit_record IS NULL THEN
                     INSERT INTO pacioli.trial_balances (subaccount, debit_balance, credit_balance, debit_changes, credit_changes, period, period_interval)
-                      VALUES (new.credit_subaccount, 0, credit_credit_balance, 0, credit_changes_amount, period_name.p, period_interval_name);
+                                                VALUES (new.credit_subaccount, 0, credit_credit_balance, 0, credit_changes_amount, period_name.p, period_interval_name);
                   ELSE
                     UPDATE pacioli.trial_balances SET credit_balance = credit_credit_balance, credit_changes = credit_changes_amount WHERE id = existing_credit_record.id;
                   END IF;
