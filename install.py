53533bc4e4e2e79809c5119afc445aa81ced094f
Pierre Rochard <pierre@rochard.org>
2016-10-23 12:31:11 -0400

    refactoring install script

--- install.py
+++ install.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import shutil
 import subprocess
@@ -7,72 +8,76 @@ import uuid
 
 python = sys.executable
 
-library_directory = os.path.join(os.path.dirname(__file__), 'libraries')
-if not os.path.exists(library_directory):
-    os.mkdir(library_directory)
 
-print subprocess.Popen(['git', 'pull'], stdout=subprocess.PIPE).stdout.read()
-print subprocess.Popen(['pip', 'install', '-r', 'instance-requirements.txt'],
-                       stdout=subprocess.PIPE).stdout.read()
-print subprocess.Popen(['pip', 'install', '--upgrade', '-r', 'instance-requirements.txt'],
-                       stdout=subprocess.PIPE).stdout.read()
-
-ofxtools_directory = os.path.join(library_directory, 'ofxtools')
-if not os.path.exists(ofxtools_directory):
-    print subprocess.Popen(['git', 'clone', 'https://github.com/PierreRochard/ofxtools', ofxtools_directory],
-                           stdout=subprocess.PIPE).stdout.read()
-else:
-    print subprocess.Popen(['git', 'pull'], stdout=subprocess.PIPE, cwd=ofxtools_directory).stdout.read()
-
-flask_security_directory = os.path.join(library_directory, 'flask-security')
-if not os.path.exists(flask_security_directory):
-    print subprocess.Popen(['git', 'clone', 'https://github.com/PierreRochard/flask-security', flask_security_directory],
-                           stdout=subprocess.PIPE).stdout.read()
-    print subprocess.Popen(['git', 'checkout', 'develop'], cwd=flask_security_directory,
-                           stdout=subprocess.PIPE).stdout.read()
-else:
-    print subprocess.Popen(['git', 'checkout', 'develop'], cwd=flask_security_directory,
-                           stdout=subprocess.PIPE).stdout.read()
-    print subprocess.Popen(['git', 'pull'], cwd=flask_security_directory,
-                           stdout=subprocess.PIPE).stdout.read()
-
-flask_admin_directory = os.path.join(library_directory, 'flask-admin')
-if not os.path.exists(flask_admin_directory):
-    print subprocess.Popen(['git', 'clone', 'https://github.com/PierreRochard/flask-admin', flask_admin_directory],
-                           stdout=subprocess.PIPE).stdout.read()
-else:
-    print subprocess.Popen(['git', 'pull'], stdout=subprocess.PIPE, cwd=flask_admin_directory).stdout.read()
+def run_command(command, cwd=None):
+    print(subprocess.Popen(command, stdout=subprocess.PIPE,
+                           cwd=cwd).stdout.read())
 
 
-print subprocess.Popen([python, 'setup.py', 'install'], stdout=subprocess.PIPE,
-                       cwd=ofxtools_directory).stdout.read()
-print subprocess.Popen([python, 'setup.py', 'install'], stdout=subprocess.PIPE,
-                       cwd=flask_security_directory).stdout.read()
-print subprocess.Popen([python, 'setup.py', 'install'], stdout=subprocess.PIPE,
-                       cwd=flask_admin_directory).stdout.read()
+def install_dependencies():
+    library_directory = os.path.join(os.path.dirname(__file__), 'libraries')
+    if not os.path.exists(library_directory):
+        os.mkdir(library_directory)
 
-pacioli_directory = os.path.join(os.path.dirname(__file__), 'pacioli')
-settings_file = os.path.join(pacioli_directory, 'settings.py')
-example_settings_file = os.path.join(pacioli_directory, 'settings_example.py')
-if not os.path.exists(settings_file):
-    shutil.copy(example_settings_file, settings_file)
-    print 'Update pacioli/settings.py and then run this script again.'
-    sys.exit(0)
+    run_command(['git', 'pull'])
+    run_command(['pip', '--no-cache-dir', 'install', '--upgrade', '-r',
+                 'instance-requirements.txt'])
 
-print subprocess.Popen([python, 'manage.py', 'createdb'], stdout=subprocess.PIPE).stdout.read()
+    custom_repos = [{'package': 'ofxtools',
+                     'url': 'https://github.com/PierreRochard/ofxtools',
+                     'branch': None},
+                    {'package': 'flask-security',
+                     'url': 'https://github.com/PierreRochard/flask-security',
+                     'branch': 'develop'},
+                    {'package': 'flask-admin',
+                     'url': 'https://github.com/PierreRochard/flask-admin',
+                     'branch': None}
+                    ]
+    for repo in custom_repos:
+        directory = os.path.join(library_directory, repo['package'])
+        if not os.path.exists(directory):
+            run_command(['git', 'clone', repo['url'], directory])
+            if repo['branch']:
+                run_command(['git', 'checkout', repo['branch']],
+                            cwd=directory)
+        else:
+            if repo['branch']:
+                run_command(['git', 'checkout', repo['branch']],
+                            cwd=directory)
+            run_command(['git', 'pull'], cwd=directory)
+        run_command([python, 'setup.py', 'install'], cwd=directory)
 
-print subprocess.Popen([python, 'manage.py', 'populate_chart_of_accounts'], stdout=subprocess.PIPE).stdout.read()
 
-admin_password = str(uuid.uuid4()).replace('-', '')
-print subprocess.Popen([python, 'manage.py', 'create_admin', '-e', 'admin@localhost', '-p', admin_password],
-                       stdout=subprocess.PIPE).stdout.read()
+def setup_settings():
+    pacioli_directory = os.path.join(os.path.dirname(__file__), 'pacioli')
+    settings_file = os.path.join(pacioli_directory, 'settings.py')
+    example_settings_file = os.path.join(pacioli_directory, 'settings_example.py')
+    if not os.path.exists(settings_file):
+        shutil.copy(example_settings_file, settings_file)
+        print('Update pacioli/settings.py and then run this script again.')
+        sys.exit(0)
 
-print 'admin@localhost password: {0}'.format(admin_password)
 
-with open('mycron', 'w') as cron_file:
-    cron_file.write('30 11,23 * * * cd {0} && python manage.py update_ofx'.format(
-            os.path.dirname(os.path.abspath(__file__))))
+def setup_database():
+    run_command([python, 'manage.py', 'createdb'])
+    run_command([python, 'manage.py', 'populate_chart_of_accounts'])
+    admin_password = str(uuid.uuid4()).replace('-', '')
+    run_command([python, 'manage.py', 'create_admin', '-e', 'admin@localhost',
+                 '-p', admin_password])
+    print('admin@localhost password: {0}'.format(admin_password))
 
-print subprocess.Popen(['crontab', 'mycron'], stdout=subprocess.PIPE).stdout.read()
 
-os.remove('mycron')
+def setup_cron():
+    directory = os.path.dirname(os.path.abspath(__file__))
+    cron_job = '30 11,23 * * * cd {0} && python manage.py ' \
+               'update_ofx'.format(directory)
+    with open('mycron', 'w') as cron_file:
+        cron_file.write(cron_job)
+    run_command(['crontab', 'mycron'])
+    os.remove('mycron')
+
+if __name__ == '__main__':
+    install_dependencies()
+    setup_settings()
+    setup_database()
+    setup_cron()
