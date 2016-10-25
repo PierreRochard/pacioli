6cb133c544d424fb1446db06a453fdb1b3f8fe3c
Pierre Rochard <pierre@rochard.org>
2016-10-25 08:34:03 -0400

    adding timezone back in

--- pacioli/models.py
+++ pacioli/models.py
@@ -25,15 +25,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
-
-    confirmed_at = db.Column(db.DateTime())
-
-    last_login_at = db.Column(db.DateTime())
-    current_login_at = db.Column(db.DateTime())
+    confirmed_at = db.Column(db.DateTime(timezone=True))
+    last_login_at = db.Column(db.DateTime(timezone=True))
+    current_login_at = db.Column(db.DateTime(timezone=True))
     last_login_ip = db.Column(db.String(255))
     current_login_ip = db.Column(db.String(255))
     login_count = db.Column(db.Integer)
-    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users'))
+    roles = db.relationship('Role',
+                            secondary=roles_users,
+                            backref=db.backref('users'))
 
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
