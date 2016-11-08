65318e7867a76c46a1d82e8a5e52770684e78020
Pierre Rochard <pierre@rochard.org>
2016-11-08 13:02:33 -0500

    reformatting

--- pacioli/views/accounting_views.py
+++ pacioli/views/accounting_views.py
@@ -4,20 +4,28 @@ from pacioli.extensions import admin
 from pacioli.models import (db, JournalEntries, Subaccounts,
                             Accounts, Classifications, Elements, TrialBalances)
 from pacioli.views import PrivateModelView
-from pacioli.views.utilities import currency_formatter, fs_currency_format, fs_linked_currency_formatter
+from pacioli.views.utilities import (currency_formatter, fs_currency_format,
+                                     fs_linked_currency_formatter)
 from sqlalchemy import text, func
 
 
 class TrialBalancesView(PrivateModelView):
     list_template = 'trial_balances.html'
-    column_list = ('id', 'period', 'period_interval', 'subaccount', 'debit_balance',
-                   'credit_balance', 'net_balance', 'debit_changes', 'credit_changes', 'net_changes')
-    column_default_sort = {'field': 'period', 'sort_desc': True, 'absolute_value': False}
+    column_list = ('id', 'period', 'period_interval', 'subaccount',
+                   'debit_balance', 'credit_balance', 'net_balance',
+                   'debit_changes', 'credit_changes', 'net_changes')
+    column_default_sort = {'field': 'period',
+                           'sort_desc': True,
+                           'absolute_value': False}
     column_searchable_list = ['subaccount']
     column_filters = column_list
     column_sortable_list = column_list
-    column_formatters = dict(debit_balance=currency_formatter, credit_balance=currency_formatter, net_balance=currency_formatter,
-                             debit_changes=currency_formatter, credit_changes=currency_formatter, net_changes=currency_formatter)
+    column_formatters = dict(debit_balance=currency_formatter,
+                             credit_balance=currency_formatter,
+                             net_balance=currency_formatter,
+                             debit_changes=currency_formatter,
+                             credit_changes=currency_formatter,
+                             net_changes=currency_formatter)
     can_edit = False
     can_create = False
     can_delete = False
@@ -27,30 +35,51 @@ class TrialBalancesView(PrivateModelView):
     def refresh_all_subaccounts(self):
         connection = db.engine.connect()
         transaction = connection.begin()
-        connection.execute('TRUNCATE bookkeeping.trial_balances RESTART IDENTITY CASCADE;')
+        connection.execute('''
+            TRUNCATE bookkeeping.trial_balances
+            RESTART IDENTITY CASCADE;
+        ''')
         transaction.commit()
         transaction.close()
-        query_text = text('SELECT bookkeeping.update_trial_balance(:debit_subaccount, :credit_subaccount, :period_interval_name, :period_name);')
-        for debit_subaccount, credit_subaccount in db.session.query(JournalEntries.debit_subaccount,
-                                                                    JournalEntries.credit_subaccount).distinct():
-            for period_interval_name in ['YYYY', 'YYYY-Q', 'YYYY-MM', 'YYYY-WW', 'YYYY-MM-DD']:
-                for period_name in db.session.query(db.func.to_char(JournalEntries.timestamp, period_interval_name)).distinct():
+        query_text = text('''
+            SELECT bookkeeping.update_trial_balance(
+                :debit_subaccount,
+                :credit_subaccount,
+                :period_interval_name,
+                :period_name);
+        ''')
+        subaccounts_query = db.session.query(
+            JournalEntries.debit_subaccount,
+            JournalEntries.credit_subaccount).distinct()
+        for debit_subaccount, credit_subaccount in subaccounts_query:
+            for period_interval_name in ['YYYY', 'YYYY-Q', 'YYYY-MM',
+                                         'YYYY-WW', 'YYYY-MM-DD']:
+                for period_name in db.session.query(
+                        db.func.to_char(JournalEntries.timestamp,
+                                        period_interval_name)).distinct():
                     transaction = connection.begin()
-                    connection.execute(query_text, debit_subaccount=debit_subaccount, credit_subaccount=credit_subaccount,
-                                       period_interval_name=period_interval_name, period_name=period_name)
+
+                    params = dict(debit_subaccount=debit_subaccount,
+                                  credit_subaccount=credit_subaccount,
+                                  period_interval_name=period_interval_name,
+                                  period_name=period_name)
+                    connection.execute(query_text, **params)
                     transaction.commit()
                     transaction.close()
         connection.close()
         return redirect(url_for('trialbalances.index_view'))
 
 
-admin.add_view(TrialBalancesView(TrialBalances, db.session, category='Accounting'))
+admin.add_view(TrialBalancesView(TrialBalances, db.session,
+                                 category='Accounting'))
 
 
 class IncomeStatementsView(PrivateModelView):
     list_template = 'financial_statements.html'
     column_list = ('subaccount', 'net_changes')
-    column_default_sort = {'field': 'net_changes', 'sort_desc': True, 'absolute_value': True}
+    column_default_sort = {'field': 'net_changes',
+                           'sort_desc': True,
+                           'absolute_value': True}
     column_searchable_list = ['subaccount']
     column_filters = column_list
     column_sortable_list = column_list
@@ -71,9 +100,12 @@ class IncomeStatementsView(PrivateModelView):
                 .join(Classifications)
                 .join(Elements)
                 .filter(self.model.net_changes != 0)
-                .filter(db.or_(Elements.name == 'Revenues', Elements.name == 'Expenses',
-                               Elements.name == 'Gains', Elements.name == 'Losses'))
-                .filter(self.model.period_interval == request.view_args['period_interval'])
+                .filter(db.or_(Elements.name == 'Revenues',
+                               Elements.name == 'Expenses',
+                               Elements.name == 'Gains',
+                               Elements.name == 'Losses'))
+                .filter(self.model.period_interval ==
+                        request.view_args['period_interval'])
                 .filter(self.model.period == request.view_args['period']))
 
     def get_count_query(self):
@@ -83,9 +115,12 @@ class IncomeStatementsView(PrivateModelView):
                 .join(Classifications)
                 .join(Elements)
                 .filter(self.model.net_changes != 0)
-                .filter(db.or_(Elements.name == 'Revenues', Elements.name == 'Expenses',
-                               Elements.name == 'Gains', Elements.name == 'Losses'))
-                .filter(self.model.period_interval == request.view_args['period_interval'])
+                .filter(db.or_(Elements.name == 'Revenues',
+                               Elements.name == 'Expenses',
+                               Elements.name == 'Gains',
+                               Elements.name == 'Losses'))
+                .filter(self.model.period_interval ==
+                        request.view_args['period_interval'])
                 .filter(self.model.period == request.view_args['period']))
 
     @expose('/')
